9ff6ad35455b286a4345ce0654baa86f1b22556b
yimu187 <yimu187@gmail.com>
2016-08-31 16:32:57 +0300

    initial commit

---
+++ SimpleWebStartUi/src/test/java/com/uniyaz/ui/awt/JnlpTest.java
@@ -0,0 +1,15 @@
+package com.uniyaz.ui.awt;
+
+import org.junit.Test;
+
+/**
+ * Created by MURAT YILMAZ on 8/31/2016.
+ */
+public class JnlpTest {
+
+    @Test
+    public void test(){
+        TestJnlp jnlp = new TestJnlp();
+
+    }
+}
